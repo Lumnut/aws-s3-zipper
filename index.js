8aab9579de5b9005968889b40b0c5be3931e63a0
Hayden Lummis <lumberry17@hotmail.com>
2018-03-01 10:06:54 +1100

    Modified file locations to allow use on AWS lamda
    - Modified file locations to allow use on AWS lamda using /tmp/ folder

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ S3Zipper.prototype = {
         }
 
         var t = this;
-        params.zipFileName = '__' + Date.now() + '.zip';
+        params.zipFileName = '/tmp/' + Date.now() + '.zip';
 
         if (params.s3ZipFileName.indexOf('/') < 0)
             params.s3ZipFileName = params.s3FolderName + "/" + params.s3ZipFileName;
@@ -345,7 +345,7 @@ S3Zipper.prototype = {
 
         var t = this;
         ///local file
-        params.zipFileName = '__' + Date.now() + '.zip';
+        params.zipFileName = '/tmp/' + Date.now() + '.zip';
 
         if (params.s3ZipFileName.indexOf('/') < 0)
             params.s3ZipFileName = params.s3FolderName + "/" + params.s3ZipFileName;
